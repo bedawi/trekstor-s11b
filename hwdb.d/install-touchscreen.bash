2d0c111f8c22fab40b6df1c581b8a985cbb21ffe
Benjamin Dahlhoff <benjamin@dahlhoff.com>
2019-12-11 18:34:56 +0100

    First try...

---
+++ hwdb.d/install-touchscreen.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
+#....
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+# Installing Trekstor S11B Touchscreen...
+echo "Installing Trekstor S11B Touchscreen..."
+cp 61-sensor-local.hwdb /etc/udev/hwdb.d/
+systemd-hwdb update
+udevadm trigger -v -p DEVNAME=/dev/iio\:device0
+echo "Done. You might have to reboot to activate device. Do not forget to check if your (Gnome) Desktop has the screen sosition locked"
